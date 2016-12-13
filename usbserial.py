6d400271354f6166a9387215aa2e786ee735c240
Mir Hossain <mir.hossain@linqia.com>
2016-12-12 17:25:53 -0800

    removed comments

--- usbserial.py
+++ usbserial.py
@@ -1,9 +1,6 @@
 from time import sleep
 import serial
-#ser = serial.Serial('/dev/ttyACM0', 9600)
-#ser.write('3')
-#sleep(2)
-#ser.write('5')
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
 	name = raw_input("press keys: ")
